e54d96dce9a524b77f850baa26edeb20db141f82
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-03-03 15:35:53 -0300

    add tokens

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 03 Mar 2023 18:30:56 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray100": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1f2937",
+  "ColorGray900": "#18181b",
+  "ColorPurple500": "#a855f7"
+};
